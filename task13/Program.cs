63c69fcaeccfda99f9d345df6faafffbbea4189b
Polina Grigorieva <polariyagv@mail.ru>
2022-07-01 20:36:30 +0500

    task13

---
+++ task13/Program.cs
@@ -0,0 +1,7 @@
+﻿int n;
+n = Convert.ToInt32(Console.ReadLine());
+if (n % 100 == n)
+Console.WriteLine("no third digit");
+else
+Console.WriteLine("third digit");
+Console.WriteLine(Convert.ToString(n)[2]);
