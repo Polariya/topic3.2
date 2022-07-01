6f0e145fafb7ffef97e243890b7dc0426eb6ae39
Polina Grigorieva <polariyagv@mail.ru>
2022-07-01 20:37:00 +0500

    task15

---
+++ task15/Program.cs
@@ -0,0 +1,8 @@
+﻿int n = Convert.ToInt32(Console.ReadLine());
+if (n%7==1) Console.WriteLine("No");  
+if (n%7==2) Console.WriteLine("No"); 
+if (n%7==3) Console.WriteLine("No"); 
+if (n%7==4) Console.WriteLine("No"); 
+if (n%7==5) Console.WriteLine("No"); 
+if (n%7==6) Console.WriteLine("Yes"); 
+if (n%7==0) Console.WriteLine("Yes"); 
